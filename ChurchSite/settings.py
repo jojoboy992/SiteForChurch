01be762d5ba8296f46a55243d9c8aceb58d7e221
jojo boy <josephedward07017@gmail.com>
2024-10-15 23:07:20 +0100

    updates

--- ChurchSite/settings.py
+++ ChurchSite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-cq4$!wd7018@ek5=95dvx744s@vtng=$a@!26#h**n8hrr_z*b')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'nice-try-lil-bro')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
